0b817994c2edba3181a42aff3aebd8ccc761a580
MariaaAT <115936415+MariaaAT@users.noreply.github.com>
2023-01-09 19:06:18 +0000

    button

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <img src="images/group.svg" alt="StarGazers Group"/>
         <h1>Meet the <i style={{color: 'steelblue'}}>{name} </i> {counter}</h1>
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <button className="outline" onClick={()=> alert('Hi There!')}>  Click Me</button>
+          <button className="outline" onClick={()=> alert('Hi There!')}>Click Me</button>
         </hgroup>
       </article>
     </div>    
