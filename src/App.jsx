a35f52ef70b4f4cb95ea64391ff9735cc38597cd
MariaaAT <115936415+MariaaAT@users.noreply.github.com>
2023-01-09 16:48:04 +0000

    size changed, example made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
+  var counter = 1;
+  counter += 1;
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
+    <hgroup> 
+      <h1>Meet the StarGazers {counter}</h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
     </hgroup>
   )
