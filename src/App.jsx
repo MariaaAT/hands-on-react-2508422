1e34bbca972bebfc9c746dbc341eeb599f9a37b3
MariaaAT <115936415+MariaaAT@users.noreply.github.com>
2023-01-10 06:19:22 +0000

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Support from './components/Support';
 
 class Welcome extends React.Component {
   constructor(props) {
@@ -24,7 +25,7 @@ function App() {
           <Hello name="StarGazers" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <Support />
         </hgroup>
       </article>
     </div>
---
+++ src/components/Support.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default () => {
+  const [count, useState] = useState(0)
+  return (
+    <button className="outline" onClick={() => setCount(count + 1)}>
+      Supported {count}
+    </button>
+  )
+}
