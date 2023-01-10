ff02a1a0b9591d9f83d1a0f7f5241589651e20a3
MariaaAT <115936415+MariaaAT@users.noreply.github.com>
2023-01-10 08:26:56 +0000

    if/else and terniary statements

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,10 +1,22 @@
 import {useState} from "react";
 
 export default () => {
-  const [count, useState] = useState(0)
+  const [count, setCount] = useState(0)
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
-      Supported {count}
+{/*This is an if/else statement
+      {(() => {
+        if(count === 0) {
+          return 'Click to support';
+        } else{
+          return `Supported ${count} times`;
+        }
+      })()
+      }
+*/}
+
+{/*TERNIARY STATEMENT*/}
+      {count === 0 ? "Click to support": `Supported ${count} times`}
     </button>
   )
 }
